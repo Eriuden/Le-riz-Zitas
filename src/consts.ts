7c56a4deb673069bbe1c73bc5aa43cb2f665366f
Eriuden <antoinecarrette@hotmail.fr>
2024-09-09 10:05:51 +0200

    first setup

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Le riz Zitas';
+export const SITE_DESCRIPTION = 'Le spécialiste de la paella';
